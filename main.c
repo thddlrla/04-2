179460bc255ffa5aa236579be4588041f4a5a1da
thddlrla <okjh0999@naver.com>
2023-09-28 01:52:36 +0900

    update main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a;
+	
+	printf("input seconds : ");
+	scanf("%i", &a);
+	
+	printf("%i seconds- %i:%i\n",a, a/60, a%60);
+	
+	system("PAUSE");
+	
+	return 0;
+}
